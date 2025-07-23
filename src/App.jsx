b01b72edfe897561293cf6f67490cf872436f250
YasserDalali <yasser.dalali.personal@gmail.com>
2025-07-23 21:33:19 +0100

    ✨ feat: add new user authentication feature with JWT support

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+
+function App() {
+  return (
+    <div>
+      <p>عمليات الإزالة والمطالبات المضادة بموجب إشعارات قانون حقوق الطبع والنشر الرقمية للألفية (DMCA)</p>
+<p>يأخذ DistroKid حقوق الملكية الفكرية على محمل الجدّ. وللحفاظ على الامتثال القانوني وأفضل الممارسات ونزاهة الخدمات، يعمل DistroKid باستمرار لضمان أنّ يقوم مستخدمينا بتحميل المحتوى الّذي يملكون حقوق نشره بنسبة 100% فقط. 
+</p>
+<p>
+ إذا كنت تعتقد بأنّ أيّ محتوى على خدماتنا ينتهك حقوق الطبع والنشر الخاصّة بك أو بأيّ شخص تمثّله، يرجى الاطّلاع على شروط الخدمة الخاصّة بنا 
+<a href="https://distrokid.com/terms/" target="_self">هنا</a>. يمكن للمدّعين التواصل مع وكيل حقوق الطبع والنشر المعيّن لدينا أو استخدام موقعنا لتقديم تقرير عن الانتهاك المزعوم لحقوق الطبع والنشر 
+<a href="https://airtable.com/shr5YDYpa1guIV81Q" target="_self">هنا</a>. 
+</p>
+<p>
+ إذا تمّ تحميل المحتوى المنتهك للحقوق باستخدام خدمة غير DistroKid، فلا يمكننا المساعدة. يمكنك في هذه الحالة التواصل مع كلّ خدمة بث بطلب إزالة عبر DMCA. 
+</p>
+<p>
+ إذا كان لديك اعتقاد صادق بأنّ محتواك أو المادّة الخاصّة بك تمّت إزالتها من DistroKid نتيجةً لخطأ في التعريف أو خطأ آخر، فيمكنك تقديم إشعار مضاد لوكيل حقوق الطبع والنشر المعيّن لدينا، كما هو محدّد في شروط الخدمة. 
+</p>
+<p>&nbsp;</p>
+<h1 id="h_01JPJXCJDYKDVFA5NMWMBV18TG">
+ لقد تمّ تحميل محتواك أو المادّة الخاصّة بك باستخدام خدمة غير DistroKid 
+</h1>
+<p>
+ إذا قام شخص ما بتحميل ملكيتك الفكرية باستخدام خدمة غير DistroKid، فهذا أمر لا يمكننا المساعدة فيه مع الأسف. ورغم ذلك، يمكنك التواصل مع كلّ خدمة بث باستخدام طلب الإزالة عبر DMCA. ربّما تحتاج إلى البحث عن الإجراءات المتّبعة لكلّ خدمة بشأن الإزالة عبر DMCA، ولكن إليك روابط للإجراءات الّتي نعرفها: 
+</p>
+<p>
+    <strong>Spotify: </strong><a href="https://www.spotify.com/us/legal/infringement-form" target="_self" dir="RTL">https://www.spotify.com/us/legal/infringement-form</a><br />
+    <strong>iTunes/Apple Music:</strong>
+    <a href="https://www.apple.com/legal/internet-services/itunes/itunesstorenotices" target="_self" dir="RTL">https://www.apple.com/legal/internet-services/itunes/itunesstorenotices</a><br />
+    <strong>YouTube:</strong>
+    <a href="https://support.google.com/legal/troubleshooter/1114905?hl=ar&amp;sjid=945387575311837502-EU" target="_self" dir="RTL">https://support.google.com/legal/troubleshooter/1114905?hl=ar&amp;sjid=945387575311837502-EU</a><br />
+    <strong>Amazon: </strong>
+    <a href="https://www.amazon.com/report/infringement" target="_self">https://www.amazon.com/report/infringement</a><br />
+</p>
+<p>&nbsp;</p>
+<h1 id="h_01JPJXG8BQGBZVMPG1BXXV8N41">
+ تمّت إزالة محتواك أو المادّة الخاصّة بك من DistroKid بسبب مطالبات حقوق الطبع والنشر 
+</h1>
+<p>وفقا لقانون الألفية للحقوق الرقمية (DMCA)، يقبل DistroKid الإشعارات المضادة لتقارير انتهاك حقوق الطبع والنشر. وبالرغم من أنّنا غير قادرين على تقدم أيّ مشورة قانونية، فإذا كان لديك اعتقاد صادق بأنّ محتواك أو المادة الخاصّة بك قد تمّت إزالتها نتيجةً لخطأ في التعريف أو خطأ آخر، يمكنك تقديم الإشعار المضاد الخاصّ بك إلى وكيل حقوق الطبع والنشر المعيّن لدينا كما هو محدّد في شروط الخدمة 
+<a href="https://distrokid.com/terms/" target="_self">هنا</a>. 
+</p>
+<p>
+ إذا قمت بتقديم معلومات مضلّلة بشأن عدم انتهاك المحتوى لأيّ حقوق أو القيام بأيّ ادّعاءات زائفة أخرى، فقد تكون مسؤولا عن أيّ أضرار (بما في ذلك التكاليف وأتعاب المحاماة). قد تؤدّي الإشعارات المضادّة الاحتيالية أو المسيئة أو أيّ استخدام مسيء لخدمات DistroKid إلى إنهاء الحساب أو عواقب قانونية أخرى. 
+</p>
+    </div>
+  )
+}
+export default App
